cb683a9964732c30336e3966deca559c016026da
Acosta <cristian.acosta01@icloud.com>
2024-02-21 16:24:00 -0300

    new page in spanish

--- app/app.js
+++ app/app.js
@@ -1,66 +1,75 @@
+//////////////////////////////DEPRECATED//////////////////////////////////////////
+
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+////////////////////////////////////////////////////////////////////////
+//////////////////////////////LANGUAJE BUTTON////////////////////////////////////////// TO DO: Improve this code
+var languageBtns = document.querySelectorAll('.language_btn');
+var iconElements = document.querySelectorAll('.iconElement');
+var languageContents = document.querySelectorAll('.language_btn-content');
 
+languageBtns.forEach(function (languageBtn, index) {
+  languageBtn.addEventListener('click', function () {
+    var languageContent = languageContents[index];
+    var currentMaxHeight = parseFloat(window.getComputedStyle(languageContent).getPropertyValue('max-height'));
+
+    if (currentMaxHeight <= 0) {
+      languageContent.style.maxHeight = '200px';
+      iconElements[index].style.transform = "rotate(180deg)";
+    } else {
+      languageContent.style.maxHeight = '0px';
+      iconElements[index].style.transform = "rotate(0deg)";
+    }
+  });
+});
+
+////////////////////////////////////////////////////////////////////////
 //////////////////////////////BURGUER MENU//////////////////////////////////////////
-
 // Get the burger button element and mobile navigation element
 const burguer_btn = document.getElementsByClassName('burguer_menu')[0];
 const mobile_nav = document.getElementById('mobile-nav');
 
-// Add a click event listener to the burger button
 burguer_btn.addEventListener('click', function () {
-  // Get the current transform value of the burger button
   let transformValue = burguer_btn.style.transform;
 
-  // Toggle the transform value between "rotate(0deg)" and "rotate(90deg)"
   if (transformValue === "rotate(90deg)") {
     burguer_btn.style.transform = "rotate(0deg)";
   } else {
     burguer_btn.style.transform = "rotate(90deg)";
   }
 
-  // Toggle the max-height value of the mobile navigation element
   if (mobile_nav.style.maxHeight) {
     mobile_nav.style.maxHeight = null;
   } else {
     mobile_nav.style.maxHeight = mobile_nav.scrollHeight + "px";
   }
 });
-
+////////////////////////////////////////////////////////////////////////
 //////////////////////////////TABS//////////////////////////////////////////
-
-// Get all the tabs sections
 const tabsSections = document.querySelectorAll('.tabs-section');
 
-// Iterate over each tabs section
 tabsSections.forEach(section => {
-  // Get the tabs and tab contents within the current section
   const tabs = section.querySelectorAll('.tabs_button');
   const tabContents = section.querySelectorAll('.tab-content-js');
 
-  // Add click event listener to each tab
   tabs.forEach(tab => {
     tab.addEventListener('click', () => {
-      // Get the data-tab attribute of the tab
       const tabId = tab.getAttribute('data-tab');
 
-      // Remove the 'tab-active' class from all tabs and 'tab-content-active' class from all tab contents in the current section
       tabs.forEach(tab => tab.classList.remove('tab-active'));
       tabContents.forEach(content => content.classList.remove('tab-content-active'));
 
-      // Add the 'tab-active' class to the clicked tab and 'tab-content-active' class to the corresponding tab content in the current section
       tab.classList.add('tab-active');
       section.querySelector(`#${tabId}`).classList.add('tab-content-active');
     });
   });
 });
-
+////////////////////////////////////////////////////////////////////////
 ///////////////////////////////TYPING EFFECT/////////////////////////////////////////
-
 // Get the elements from the DOM
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
@@ -149,30 +158,26 @@ checkboxes.forEach(checkbox => {
     });
   });
 });
-
+////////////////////////////////////////////////////////////////////////
 ///////////////////////////////EMAIL JS/////////////////////////////////////////
-
 const btn = document.getElementById('button');
 document.getElementById('form').addEventListener('submit', function (event) {
   event.preventDefault();
 
-  btn.value = 'Sending...';
+  let btnlang = btn.dataset.lang;
 
-  // Specify the email service and template IDs
+  btn.value = btnlang === "es" ? 'Enviando...' : 'Sending...';
+  btn.disabled = true;
+
   const serviceID = 'default_service';
   const templateID = 'template_69w3aa3';
 
-  // Send the form using emailjs
   emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      // On success, update button value and show success alert
-      btn.value = 'Sent!';
-      alert('Sent!');
+      btn.value = btnlang === "es" ? 'Enviado!' : 'Sent!';
     }, (err) => {
-      // On error, update button value and show error alert
-      btn.value = 'Send Email';
+      btn.value = btnlang === "es" ? 'Enviar email' : 'Send email';
       alert(JSON.stringify(err));
     });
 });
-
 ////////////////////////////////////////////////////////////////////////
